0763c2647ed06b2fcd6a47440610c758c9048c4b
02071819 <54053565+02071819@users.noreply.github.com>
2021-10-07 02:26:13 +0800

    Add files via upload

---
+++ graduateProject/admin/inc/connection.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','graduateDB');
+
+// if($conn){
+//     echo "Connection successfully";
+// }
+?>
---
+++ graduateProject/admin/left.inc.php
@@ -0,0 +1,7 @@
+<!-- index左欄位 -->
+<div class="leftDiv">
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="category.php">Category Manage</a></li>
+    </ul>
+</div>
