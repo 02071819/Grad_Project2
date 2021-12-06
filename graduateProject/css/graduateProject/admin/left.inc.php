aa5f5ddab6e6df2f131b8afa991f081fc593bbe4
02071819 <54053565+02071819@users.noreply.github.com>
2021-12-07 01:11:50 +0800

    Delete left.inc.php

--- graduateProject/css/graduateProject/admin/left.inc.php
+++
@@ -1,8 +0,0 @@
-<!-- index左欄位 -->
-<div class="leftDiv">
-    <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="category.php">Category Manage</a></li>
-        <li><a href="products.php">Product Manage</a></li>
-    </ul>
-</div>
